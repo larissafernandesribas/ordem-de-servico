4d52211b493e0b79c4492bcf3b167d48be81f10b
larissafernandesribas <larissafernandesribas@gmail.com>
2025-04-23 09:44:10 -0300

    Fim da Etapa 3

--- src/main/java/br/kge/eti/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/kge/eti/OSApiApplication/api/controller/ClienteController.java
@@ -1,31 +1,85 @@
-    /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.kge.eti.OSApiApplication.api.controller;
+
 import br.kge.eti.OSApiApplication.domain.model.Cliente;
 import br.kge.eti.OSApiApplication.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author sesideva
  */
-
 @RestController
 public class ClienteController {
-    
+
     @Autowired
     private ClienteRepository clienteRepository;
-            
+
     @GetMapping("/clientes")
-    public List<Cliente> listas(){
-    return clienteRepository.findAll(); 
+    public List<Cliente> listas() {
+
+        return clienteRepository.findAll();
+        //return clienteRepository.findByNome("Lerys");
+        //return clienteRepository.findByNomeContaining("Silva");
+    }
+
+    @GetMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteID) {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteID);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteID,
+            @RequestBody Cliente cliente) {
+        
+        if (!clienteRepository.existsById(clienteID)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteID);
+        cliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    
+    @DeleteMapping("/clientes/{clienteID}")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteID){
+        
+        if (!clienteRepository.existsById(clienteID)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        clienteRepository.deleteById(clienteID);
+        return ResponseEntity.noContent().build();
     }
 }
-
